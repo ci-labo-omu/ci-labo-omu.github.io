9cb69421da71237f22b77a04901a90fe6b44680b
TakatoKinoshita <sbb01065@st.osakafu-u.ac.jp>
2022-09-14 01:55:12 +0900

    from CI-labo-OPU/omu-cil.github.io edit index.html

---
+++ assets/js/slidedown.js
@@ -0,0 +1,116 @@
+
+
+
+/*
+	$(function() {
+		var bln = $('#qa1');bln.hide();
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 500) {
+				bln.slideDown(1500);}}
+		);
+	});
+*/
+
+
+	$(function() {
+		var bln = $('#qa2');bln.hide();
+		var height = $(window).height();
+		var monitor = screen.height;
+		var sa = monitor - height;
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 80+sa) {
+				bln.slideDown(1500);}
+		});
+	});
+/*
+	$(function() {
+		var bln = $('#qa3');bln.hide();
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 660) {
+				bln.slideDown(1500);}
+		});
+	});
+*/
+	$(function() {
+		var bln = $('#qa4');bln.hide();
+		var height = $(window).height();
+		var monitor = screen.height;
+		var sa = monitor - height;
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 120+sa) {
+				bln.slideDown(1500);}
+		});
+	});
+/*
+	$(function() {
+		var bln = $('#qa5');bln.hide();
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 830) {
+				bln.slideDown(1500);}
+		});
+	});
+*/
+	$(function() {
+		var bln = $('#qa6');bln.hide();
+		var height = $(window).height();
+		var monitor = screen.height;
+		var sa = monitor - height;
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 160+sa) {
+				bln.slideDown(1500);}
+		});
+	});
+/*
+	$(function() {
+		var bln = $('#qa7');bln.hide();
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 1000) {
+				bln.slideDown(1500);}
+		});
+	});
+*/
+	$(function() {
+		var bln = $('#qa8');bln.hide();
+		var height = $(window).height();
+		var monitor = screen.height;
+		var sa = monitor - height;
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 200+sa) {
+				bln.slideDown(1500);}
+		});
+	});
+/*
+	$(function() {
+		var bln = $('#qa9');bln.hide();
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 1150) {
+				bln.slideDown(1500);}
+		});
+	});
+*/
+	$(function() {
+		var bln = $('#qa10');bln.hide();
+		var height = $(window).height();
+		var monitor = screen.height;
+		var sa = monitor - height;
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 240+sa) {
+				bln.slideDown(1500);}
+		});
+	});
+/*
+	$(function() {
+		var bln = $('#qa11');bln.hide();
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 1300) {
+				bln.slideDown(1500);}
+		});
+	});
+*/
+	$(function() {
+		var bln = $('#qa12');bln.hide();
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 280) {
+				bln.slideDown(600);}
+		});
+	});
